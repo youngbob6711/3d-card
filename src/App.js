96778fd29976eaef75757a81778444e0932feb7f
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-10-26 18:33:25 +0300

    added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Card from './components/Card';
 const App = () => {
   return (
     // card
-    <div className='w-full h-screen flex justify-center items-center px-10'>
+    <div className='w-full h-screen flex justify-center items-center px-10 bg-gradient-to-tl from-[#EAFDD3]/90 to-[#BAEDC8]'>
       <Card />
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PumpkinImg from '../img/pumpkin.svg';
 
-// import motion
 import { motion, useMotionValue, useTransform } from 'framer-motion';
 
 const Card = () => {
@@ -15,21 +14,22 @@ const Card = () => {
       <motion.div
         style={{ x, y, rotateX, rotateY, z: 100 }}
         drag
-        dragElastic={0.16}
+        dragElastic={0.18}
         dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
         whileTap={{ cursor: 'grabbing' }}
         className='w-[400px] h-[520px] rounded-[30px] bg-gradient-to-tl from-[#0C3720]/90 to-[#5B874B] cursor-grab'
       >
         <div className='w-full h-full flex justify-center items-center'>
           <div className='flex flex-col justify-center items-center'>
+            {/* bg */}
             <div className='bg-skull w-[600px] h-[600px] bg-cover'></div>
-            <h2 className='text-[48px] text-white absolute top-10 uppercase font-extrabold text-center leading-tight max-w-[320px]'>
+            <h2 className='text-[48px] text-white absolute top-10 uppercase font-black text-center leading-tight max-w-[330px]'>
               UOOOO TRICK OR TREAT!!
             </h2>
             <motion.div
               style={{ x, y, rotateX, rotateY, z: 100000 }}
               drag
-              dragElastic={0.12}
+              dragElastic={0.14}
               whileTap={{ cursor: 'grabbing' }}
               className='absolute top-48'
             >
